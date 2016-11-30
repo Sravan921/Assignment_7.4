7368fbafccb5ea6702b4c0e29946083fe0ee0f98
Sravan921 <sukhavasi92@gmail.com>
2016-11-30 13:58:10 -0600

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// It will be used for taking arguments  from inner to outer functions.
+
 function outerFunction(innerFunction) {
   if (innerFunction && (typeof innerFunction == "function")) {
       innerFunction();
   }
 }
 
+// It will display the Message from Inner Function
 function innerFunction()
 {
     console.log("Hello World from Inner Function");
